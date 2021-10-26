477ebfdb5de6c3d0b941fd7078d68510ae669746
michalmendygral <44322173+michalmendygral@users.noreply.github.com>
2021-10-26 13:59:43 +0200

    Update temp.py

--- python101/temp.py
+++ python101/temp.py
@@ -1,8 +1,8 @@
 #
 #
-# Taking CSV in S3, counting the number of rows, send the corresponding KPI onto ES
+# Taking CSV in S3, counting the number of rows
 #
-# Authors: Michal Mendygral, Denis Arnaud
+# Authors: Michal Mendygral
 #
 #
 from datetime import date, datetime, timedelta
@@ -10,26 +10,7 @@ import datetime
 import bz2
 from itertools import (takewhile, repeat)
 
-#TODAY = str(datetime.today())
-#TODAY_MONTH = str(datetime.today())[:-3]
-#TODAY_YEAR = str(datetime.today()).split("-")[0]
 
-# Exchange S3 bucket
-#S3_URL_ROOT = "https://console.aws.amazon.com/s3/buckets/analytics-pr-s3-exchange/lake/ocean/volumes/aor"
-#S3_URL_SUFFIX = "?region=eu-central-1&tab=overview#"
-#S3_URL= S3_URL_ROOT + "/" + TODAY_YEAR + "/" + TODAY_MONTH + "/" + TODAY + "/" + S3_URL_SUFFIX
-#S3_DIR = "/data/exchange/ocean/volumes/aor/" + TODAY_YEAR + "/" + TODAY_MONTH + "/" + TODAY + "/"
-#S3_FILE = S3_DIR+"aor_volume_extract-"+TODAY+".csv.bz2"
-
-#print(S3_FILE)
-#content =""
-#with bz2.open("/Users/michalmendygral/Downloads/aor_volume_extract-2020-06-15.csv.bz2", "rb") as f:
-#    content = len(f.readlines())
-#    print(content)
-
-#with open("/Users/michalmendygral/Downloads/aor_volume_extract-2020-06-15.csv", "rb") as f:
-#    content = len(f.readlines())
-#    print(content)
 
 x = "somevalue"
 
@@ -48,8 +29,8 @@ print(x)
 func_A()
 func_B()
 
-S3 ="/data/bdp-all-shared/data/analytics/lake/ocean/volumes/aor/2020/2020-06/2020-06-19/aor_shpm_extract-2020-06-19.csv.bz2"
-filename  = "/Users/michalmendygral/Downloads/aor_volume_extract-2020-06-19.csv.bz2"
+S3 ="/data/2020/2020-06/2020-06-19/extract-2020-06-19.csv.bz2"
+filename  = "~/Downloads/extract-2020-06-19.csv.bz2"
 def func_temp():
     return 12, (S3.split("/")[12]).split("-")[0]
 
